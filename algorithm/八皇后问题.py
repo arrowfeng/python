481302c12fb0d2f71ffaa88628fdd6279f07e3f9
zdf <289716347@qq.com>
2018-07-10 15:32:42 +0800

    添加打印辅助函数

--- algorithm/八皇后问题.py
+++ algorithm/八皇后问题.py
@@ -1,4 +1,5 @@
 
+import random
 #state[0]=1表示是在第1行第2列已经有皇后了
 def conflict(state,nextX):
 	nextY = len(state)#行
@@ -11,13 +12,16 @@ def queens(num=8, state=()):
 	for pos in range(num):
 		if not conflict(state,pos):
 			if len(state) == num - 1:
-				print('pos',pos)
 				yield (pos,)
 			else:
 				for result in queens(num, state + (pos,)):
-					print('pos',pos)
-					print('result',result)
 					yield (pos,) + result
 
-print(list(queens(4)))
+#打印助手函数
+def prettyprint(solution):
+	def line(pos,length=len(solution)):
+		return '.'*(pos) + 'X' +'.'*(length-pos-1)
+	for pos in solution:
+		print(line(pos))
 
+prettyprint(random.choice(list(queens(8))))
