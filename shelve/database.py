574f637cd29197eb3a3b80e7f1d168d31b4db26b
zdf <289716347@qq.com>
2018-07-11 11:33:33 +0800

    add shelve

---
+++ shelve/database.py
@@ -0,0 +1,58 @@
+#database.py
+import sys, shelve
+
+def store_person(db):
+	"""
+	
+	Query user for data and store it in the shelf objecy
+	"""
+
+	pid = input('Enter unique ID number: ')
+	person={}
+	person['name']=input('Enter name: ')
+	person['age']=input('Enter age: ')
+	person['phone']=input('Enter phone number: ')
+
+	db[pid] = person
+
+
+def lookup_person(db):
+	"""
+	Query user for ID and desired field, and fetch the corresponding data from the shelf object
+	"""
+
+	pid = input("Enter ID number: ")
+	field = input('What would you like to know?(name,age,phone)')
+	field = field.strip().lower()
+	print(field.capitalize()+':',db[pid][field])
+
+def print_help():
+	print('The availabel commands are: ')
+	print('store : Stores information about a person')
+	print('lookup: Looks up a person from ID number')
+	print('quit  : Save changes and exit')
+	print('?     : Prints this message')
+
+def enter_cmd():
+	cmd = input('Enter command(? for help): ')
+	cmd = cmd.strip().lower()
+	return cmd
+
+def main():
+	database = shelve.open('database.dat')
+	print('s')
+	try:
+		while True:
+			cmd = enter_cmd()
+			if cmd == 'store':
+				store_person(database)
+			elif cmd == 'lookup':
+				lookup_person(database)
+			elif cmd == '?':
+				print_help()
+			elif cmd == 'quit':
+				return
+	finally:
+		database.close()
+
+if __name__ == '__main__':main()
